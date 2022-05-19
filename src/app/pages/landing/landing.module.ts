3c5eb7780a6926414741387a878c072e25d4ca32
ilyass.elfouih <ilyass.elfouih@gmail.com>
2022-05-19 04:03:25 +0100

    feat(landing): :sparkles: landing page with background img & btn

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, LandingRoutingModule],
+  imports: [CommonModule, LandingRoutingModule, SharedModule],
 })
 export class LandingModule {}
